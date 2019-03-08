12e37a4504b7038fcb4c56226d743c7e446775fb
Michael Francoeur <michael.francoeur@ibm.com@michaels-mbp-2.dfw.ibm.com>
2019-03-08 14:07:11 -0500

    initial commit

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const { NODE_ENV } = process.env;
+
+// Middleware
+app.use(bodyParser.json());
+
+// Serve static files on behalf of the client
+app.use(express.static(path.join(__dirname, '..', 'build')));
+
+// Handle React routing by returning all requests to client
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+});
+
+app.listen(PORT, () => console.log(`Proxy server listening on port ${PORT}!`));
